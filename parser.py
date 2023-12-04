6c71aa1160d7000695de483ff9e9e0388b3d1c9e
Jeod <47716344+JeodC@users.noreply.github.com>
2023-12-03 19:23:38 -0500

    Update parser.py
    Update output to look cleaner

--- parser.py
+++ parser.py
@@ -29,7 +29,7 @@ def parse_gamelist(xml_file, output_file):
 
             # If there are missing tags, add an entry to the results list
             if missing_tags:
-                results.append(f"{name} found in {xml_file} but is missing {', '.join(missing_tags)}.")
+                results.append(f"{name} found with missing tags: {', '.join(missing_tags)}.")
 
     # If there are results, write them to an output file
     if results:
@@ -38,7 +38,7 @@ def parse_gamelist(xml_file, output_file):
 
 def scan_directory():
     # Prompt user for the directory path
-    directory = input("Enter the directory path: ")
+    directory = input("Enter the path to your roms folder: ")
 
     # Check if the directory exists
     if not os.path.exists(directory):
